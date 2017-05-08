c6dcb043ed6ac97f0f6ff56d644e37828028eaa0
Kanyinsola Fapohunda Oyindamola <lilkaydeee@gmail.com>
2017-05-08 09:37:35 +0100

    Add New Feature: Order Summary

--- app/src/main/java/com/example/codedentwickler/andelealc_beginners/MainActivity.java
+++ app/src/main/java/com/example/codedentwickler/andelealc_beginners/MainActivity.java
@@ -25,16 +25,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
+
         EditText nameEditText = (EditText) findViewById(R.id.name_edit_text);
         EditText emailEditText = (EditText) findViewById(R.id.email_edit_text);
 
         String name = nameEditText.getText().toString().trim();
         String[] email = new String[] {emailEditText.getText().toString().trim()};
+
         int priceOfOneCoffeeCup = 25;
-        displayPrice(quantity * priceOfOneCoffeeCup);
 
         String sugar = withSugar ? "Yes" : "No";
         String milk = withMilk ? "Yes" : "No";
+
+        displayPrice(quantity * priceOfOneCoffeeCup);
 
         String message = "Name: "+name + "\nWanted Sugar: "+sugar
                 + "\nWanted Milk: "+milk + "\nQuantity: " +quantity
@@ -54,7 +57,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, R.string.enter_name_email_toast_message,Toast.LENGTH_LONG).show();
         }
-
     }
 
     private void display(int number) {
